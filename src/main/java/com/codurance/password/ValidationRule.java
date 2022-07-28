b7cf74c5b9851a58213db1e8b6e825c1700e6a3e
Mauro Chojrin <mauro.chojrin@codurance.com>
2022-07-28 16:13:39 +0200

    Unit test failing for the right reason

--- src/main/java/com/codurance/password/ValidationRule.java
+++ src/main/java/com/codurance/password/ValidationRule.java
@@ -1,5 +1,11 @@
 package com.codurance.password;
 
 public abstract class ValidationRule {
+    private String errorMessage;
+
     public abstract boolean isValid(String password);
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
 }
--- src/test/java/com/codurance/password/PasswordValidatorShould.java
+++ src/test/java/com/codurance/password/PasswordValidatorShould.java
@@ -1,24 +1,35 @@
 package com.codurance.password;
 
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class PasswordValidatorShould {
     private final PasswordValidator passwordValidator = new PasswordValidator();
+    @Mock
+    ValidationRule validation;
 
     @ParameterizedTest
     @CsvSource({
-            "1bC_efgh, true",
-            "1bC_efg, false",
-            "1bc_efgh, false",
-            "1BC_EFGH, false",
-            "AbC_efgh, false",
-            "1bCdefgh, false",
+            "1bC_efgh,Custom error",
     })
+    public void return_errors_according_to_its_validation_rules(String password, String error) {
+        when(validation.isValid(anyString())).thenReturn(false);
+        doAnswer(invocation -> error).when(validation).getErrorMessage();
 
-    public void detect_incorrect_passwords(String password, boolean isValid) {
-        assertEquals(isValid, passwordValidator.isValid(password));
+        passwordValidator.addValidationRule(validation);
+
+        assertEquals(Arrays.asList(error), passwordValidator.getProblemsWith(password));
+        verify(validation).isValid(eq(password));
     }
 }
